62773e7a78cfc6dc1753da3e0ab45884a2255013
NadiaXY <gbnadia025@gmail.com>
2024-04-15 15:54:59 +0200

    ViewModel + Basic Fragments + Nav Graph

---
+++ app/src/main/java/com/example/beautynetwork/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.beautynetwork
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
