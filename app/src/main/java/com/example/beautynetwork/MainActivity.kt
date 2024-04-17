3aae193cc7c9bef80711a091a00a0987ed1a1639
NadiaXY <gbnadia025@gmail.com>
2024-04-17 17:34:05 +0200

    SplashScreen

--- app/src/main/java/com/example/beautynetwork/MainActivity.kt
+++ app/src/main/java/com/example/beautynetwork/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Thread.sleep(5000)
+        Thread.sleep(6000)
         installSplashScreen()
 
         //Erstellt ein Binding-Objekt für die Aktivität mithilfe des generierten Binding-Klasse ActivityMainBinding.
